4e6760da0014d31780d2d27e4bf3c4850df2e465
tps-80 <thomaspsorensen@gmail.com>
2015-10-08 10:35:13 -0700

    Implement functional shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,34 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {
+    length : 0,
+    storage : {}
+  };
+  
+  extend(obj, stackMethods);
+  return obj
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size : function() {
+    return this.length;
+  },
+  push : function(value) {
+    this.length++;
+    this.storage[this.length] = value;
+  },
+  pop : function() {
+    if (this.length >0) {
+      var temp = this.storage[this.length];
+      this.length--;
+      delete this.storage[this.length];
+    }
+    return temp;
+  }
+};
 
-
+var extend = function(original, extendobj) {
+  for (var attrname in extendobj) 
+    { original[attrname] = extendobj[attrname];}
+}
