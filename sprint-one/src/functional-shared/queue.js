40ec44db8a4a086ce36742a36bf3e5fe57c73c37
tps-80 <thomaspsorensen@gmail.com>
2015-10-08 11:08:51 -0700

    Implement functional shared Queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,36 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
+  var obj = {
+    length : 0,
+    max : 0,
+    dequeueCount : 0,
+    storage : {}
+  };
+  extend(obj, queueMethods);
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size : function() {
+    return this.length;
+  },
+  enqueue : function(value) {
+    this.length++;
+    this.max++;
+    this.storage[this.max] = value;
+  },
+  dequeue : function() {
+    if (this.length > 0){
+      var temp = this.storage[1 + this.dequeueCount];
+      delete this.storage[1 + this.dequeueCount];
+      this.length--;
+      this.dequeueCount++;
+      return temp;
+    }
+  }
+};
 
-
+var extend = function(original, extendobj) {
+  for (var attrname in extendobj) 
+    { original[attrname] = extendobj[attrname];}
+};
