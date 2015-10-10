13cb52bed3025ab6376631c6b64baeff5b936efa
tps-80 <thomaspsorensen@gmail.com>
2015-10-09 20:24:23 -0700

    implemented linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,41 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = new Node(value);
+
+    if (this.tail) { //this is first time through, no tail
+      //this.tail = newNode;
+      //this.head = newNode;
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    else { //first tail add is also a head add (this is case where tail already exists)
+      this.head = newNode; //head will have no .next value
+      this.tail = newNode;
+      //this.tail.next = newNode;
+      //this.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    var temp = this.head;
+    this.head = temp.next;
+    return temp.value; 
   };
 
   list.contains = function(target) {
+    var containsRecursion = function(node) {
+      if (node.value === target) {
+        return true;
+      } else {
+        if (!node.next) {
+          return false;
+        } else {
+          return containsRecursion(node.next);
+        }
+      }
+    }
+    return containsRecursion(this.head);
   };
 
   return list;
