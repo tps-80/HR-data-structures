cb6ae64d71884703ac9e03ad737353871463b0c6
tps-80 <thomaspsorensen@gmail.com>
2015-10-09 21:41:12 -0700

    working on tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,24 +1,38 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  extend(newTree,treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  // your code here
-  newTree.children = null;  // fix me
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+
+  var containsRecursion = function(tree) {
+    if (tree.value === target) {
+      return true;
+    } 
+    else {
+      for (var i =0; i < tree.children.length; i++) {
+        return containsRecursion(tree.children[i]);
+      }
+    }
+    return false;
+  }
+  return containsRecursion(this);
 };
 
 
+var extend = function(original, extendobj) {
+  for (var attrname in extendobj) 
+    { original[attrname] = extendobj[attrname];}
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
